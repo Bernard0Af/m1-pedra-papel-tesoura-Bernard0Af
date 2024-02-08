f70ad3286633724c773e0043e4a4054a72055e6d
Bernardo A <araujobernardo11111@gmail.com>
2024-02-08 10:28:54 -0300

    pedra-papel-teoura

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 /* Desenvolva seu código abaixo */
-console.log("Primeira entrega");
+function playRockPaperScissor(player1, player2) {
+    let play1 = player1.toLowerCase(); 
+    let play2 = player2.toLowerCase();
+    
+    if(play1 == play2){
+        return "Empate!"
+    }else if(play1 == "pedra") {
+        if(play2 == "papel") {
+            return "Jogador 2 venceu!"
+        }else if(play2 == "tesoura") {
+            return "Jogador 1 venceu!"
+        }
+    }else if (play1 == "papel") {
+        if(play2 == "pedra") {
+            return "Jogador 1 venceu!"
+        }else if(play2 = "tesoura") {
+            return "Jogador 2 venceu!"
+        }
+    }else if (play1 == "tesoura") {
+        if (play2 == "pedra") {
+            return "Jogador 2 venceu!"
+        }else {
+            (play2 == "papel")
+            return "Jogador 1 venceu!"
+        }
+    }
+}
+
+ const result = playRockPaperScissor("PEDRA", "Tesoura")
+ console.log(result);
