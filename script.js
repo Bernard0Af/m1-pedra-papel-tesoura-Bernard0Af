0763452b9e3ca3d70919819cd4ee147e5710beab
Bernardo A <araujobernardo11111@gmail.com>
2024-02-08 21:40:50 -0300

    Correção

--- script.js
+++ script.js
@@ -3,16 +3,23 @@ function playRockPaperScissor(player1, player2) {
     let play1 = player1.toLowerCase(); 
     let play2 = player2.toLowerCase();
     
-    if(play1 === play2) {
-        return "Empate!"
-    }else if (play1 === "pedra" && play2 === "papel" || play1 === "papel" && play2 === "tesoura" || play1 === "tesoura" && play2 === "pedra") {
+    if ((play1 === "pedra" && play2 === "papel") ||
+        (play1 === "papel" && play2 === "tesoura") ||
+        (play1 === "tesoura" && play2 === "pedra")
+        ){
         return "Jogador 2 venceu!"
-    }else {
-        (play2 === "pedra" && play1 === "papel" || play2 === "papel" && play1 === "tesoura" || play2 === "tesoura" && play1 === "pedra")
+    }else if((play2 === "pedra" && play1 === "papel") ||
+            (play2 === "papel" && play1 === "tesoura") ||
+            (play2 === "tesoura" && play1 === "pedra")
+            ){
             return "Jogador 1 venceu!"
+    }else {
+        return "Empate!"
     }
 }
 
 
  const result = playRockPaperScissor("PEDRA", "papel")
  console.log(result);
+
+ 
